3d29f73df91cc0e625442b5e7bbdfa5c75dcc970
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-14 15:47:03 +0500

    variables

---
+++ 01-variables/main.js
@@ -0,0 +1,71 @@
+// console.log se hum user ko koi msg show kr skte hain ye print 
+// console.log("Hello how are");
+
+// comment code ka woh part jo execute nhii hota (readability ke liye use hota he )
+// 1. single line comment 
+// this is single line comment 
+
+// 2. multi line comment 
+/*
+this is multi line comment 
+*/
+
+//  VARIABLES => variables are container that stores data.
+
+// three stages of variable :
+// 1. declaration => let name;
+// 2. initialization=> name = "sam";
+// 3. use => console.log(name);
+
+// let age;
+// age = 22;
+// console.log(age);
+
+// let name = "ahmad";
+// name = "sam"
+// console.log(name);
+
+// LET, CONST , VAR 
+// 1. var => var can be re declared and updated. global scope variable.
+// var x = 10;
+// var x = 20;
+// console.log(x);
+// 2. let => variable cannot be re declared but updated. A block scope.
+// let a = 10;
+// let a = 20;
+// console.log(a);
+// 3. const => variable cannot be re declared and updated. A block scope.
+// const pi = 3.14;
+// pi = 3.15;
+// console.log(pi);
+
+// RULES FOR NAMING VARIABLES 
+// 1. variable name are case sensitive.
+// 2. only letters , digits, underscore (_), $ is allowed. (Not even space);
+// 3. Reserved words cannot be variables names.
+// 4. only letter , underscore , or $ should be first character.
+
+
+//  PRIMITIVE DATA TYPES (NNSSBBU)
+// let price1 = 299;
+// let price2 = 399;
+// let sum = price1 + price2;
+// console.log(sum);
+
+
+// let firstName = prompt("Enter a name");
+// let lastName = prompt("Enter last name");
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+// javascript is dynamically typed language and forgiving language 
+// you don't need to specify the type of variable 
+// let a = 1;
+// a = "ali"
+// a = true;
+// console.log(typeof a);
+// console.log(typeof (a)); 
+
+console.log("2" + 2);
+console.log("2" - 2);
