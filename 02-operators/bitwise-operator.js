8a18d3287b57a499e97469f867c5b5a7cb3808cb
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-16 00:56:46 +0500

    bitwise operator

---
+++ 02-operators/bitwise-operator.js
@@ -0,0 +1,12 @@
+// bitwise &
+// console.log(5 & 3);
+// console.log(4 & 7);
+// console.log(5 & 3);
+
+// bitwise |
+// console.log(8 | 11);
+// console.log(26 | 15);
+
+// bitwise ~
+// console.log(~3);
+// console.log(~0);
