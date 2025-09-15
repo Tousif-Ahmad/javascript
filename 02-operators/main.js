abb3f4c97f15c66bc24015d3d4f82bdac50fa6e3
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-15 16:13:45 +0500

    Basic Operators

---
+++ 02-operators/main.js
@@ -0,0 +1,65 @@
+// OPERATORS => special symbol that tells the computer to do something with  values.
+
+// 1. ARITHMETIC OPERATOR 
+// console.log(2 + 3);
+// console.log(3 - 2);
+// console.log(10 / 2);
+// console.log(10 % 2);
+// console.log(3 * 2);
+// console.log(3 ** 3);
+
+// challenge 1.
+// let price = 150;
+// let quantity = 3;
+// let totalCost = price * quantity;
+// let discount = totalCost * 0.10;
+// let discountedPrice = totalCost - discount;
+
+// console.log("price per item ", price);
+// console.log("Quantity", quantity);
+// console.log("Total Cost ", totalCost);
+// console.log("Discount (10%)", discount);
+// console.log("Discounted Price", discountedPrice);
+
+
+// 2. ASSIGNMENT OPERATOR 
+// let a = 5;
+// a+=5;
+// console.log(a);
+// a-=10;
+// console.log(a);
+// a*=2;
+// console.log(a);
+// a%=10;
+// console.log(a);
+
+
+// 3. COMPARISON OPERATOR 
+// console.log(3 > 1);
+// console.log(3 >= 3);
+// console.log(4 < 1);
+// console.log(8 <= 8);
+// console.log(3 == 3);
+// console.log("2" == "2");
+// console.log(1 === 1);
+// console.log(1 === "1");
+// console.log(2 != 2);
+// console.log("2" !== 2);
+
+// 4. LOGICAL OPERATOR 
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+// console.log(!true);
+// console.log(!false);
+
+
+// let x = 8;
+// console.log(x >=0 && x <=100);
