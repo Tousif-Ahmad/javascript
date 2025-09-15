d2873cd70fb1ed03064de97bc40ec2c2b7406c1d
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-16 00:25:13 +0500

    unary and ternary operator

---
+++ 02-operators/unary-operator.js
@@ -0,0 +1,36 @@
+// increment & decrement => quick way to increase and decrease a value.
+// let a = 4;
+// console.log(a);
+// console.log(a++);
+// console.log(a); /* 5 */
+// console.log(--a);
+// console.log(a--);
+// console.log(a);  /* 3 */
+
+// let a = 10;
+// let result = a++ + ++a -10;
+// console.log(result);
+
+
+// TERNARY OPERATOR 
+// let age = 12;
+// let result = age >= 18 ? "You can drive" : "You can't drive";
+// console.log(result);
+
+// challenge 1.
+// let age = 18;
+// let ans = age >= 18 ? "Eligible for booking" : "Not Eligible for booking";
+// console.log(ans);
+
+// challenge 2.
+// let username = "ali";
+// let password = "jdklasjfi77";
+// let res = (username && password) ? "Login Successful" : "Both Fields are required";
+// console.log(res);
+
+// find largest number among 3
+// let a = 2;
+// let b = 42;
+// let c = 6;
+// let ans = a > b ? (a > c ? a : c) : (b > c ? b : c);
+// console.log(ans);
