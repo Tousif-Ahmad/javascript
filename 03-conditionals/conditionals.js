d7f3e2b4ca9d6064eaca34d01650279bdf69ac20
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-16 15:50:32 +0500

    conditionals

---
+++ 03-conditionals/conditionals.js
@@ -0,0 +1,106 @@
+// console.log("Conditional statements");
+// A conditional statement let's your code make decisions if something is true or false.
+
+// let productStock = 2;
+// if (productStock === 0){
+//     console.log("Product is out of  stock");
+// }
+// else{
+//     console.log("Product in stock");
+// }
+
+
+
+// let weather = "sunny";
+
+// if (weather === "rainy"){
+//     console.log("take umbrella");
+// }
+// else{
+//     console.log("Enjoy the sun..");
+// }
+
+// let score = 88;
+// if (score >= 90){
+//     console.log("Grade A");
+// }
+// else if (score >= 80){
+//     console.log("Grade B");
+// }
+// else if (score >= 70){
+//     console.log("Grade C");
+// }
+// else{
+//     console.log("You need to study more..");
+// }
+
+// NESTED IF ELSE 
+// let age = 22;
+// let hasId = true;
+
+// if (age >= 18){
+//     if (hasId){
+//         console.log("You are allowed to go.");
+//     }
+//     else{
+//         console.log("You need an Id");
+//     }
+// }
+// else{
+//     console.log("You are two young.");
+// }
+
+
+// SWITCH STATEMENT 
+// let color = "red";
+// switch(color){
+//     case "red":
+//         console.log("Stop");
+//         break;
+//     case "yellow":
+//         console.log("Cautions");
+//         break;
+//     case "green":
+//         console.log("Go");
+//         break;
+//     default:
+//         console.log("Unknown Color");
+// }
+
+// challenge  1.
+// let cartValue = 20;
+// let totalCartValue;
+
+// if (cartValue < 50){
+//     totalCartValue = cartValue;
+//     console.log("No discount applied");
+// }
+// else if (cartValue >= 50 && cartValue <= 100){
+//     totalCartValue = cartValue - (cartValue * 0.10);
+//     console.log("A 10% discount has been applied.");
+// }
+// else {
+//     totalCartValue = cartValue - (cartValue * 0.20);
+//     console.log("A 20% discount applied.");
+// }
+// console.log(`A total cart value is ${totalCartValue.toFixed(2)}`);
+
+// // challenge 2.
+// let hasSubscription = true;
+// let subscriptionType = "premium";
+
+// if (hasSubscription){
+//     if (subscriptionType === "premium"){
+//         console.log("Access to all content");
+//     }
+//     else if (subscriptionType === "standard"){
+//         console.log("Access to limited content");
+//     }
+//     else{
+//         console.log("Unknown subscription type");
+//     }
+// }
+// else{
+//     console.log("Please subscribe to access content");
+// }
+
