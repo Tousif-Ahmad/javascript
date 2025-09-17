2565074ef034bdc66f658bde87fadaba672d9876
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-18 00:58:18 +0500

    strings

---
+++ 06-strings/strings.js
@@ -0,0 +1,60 @@
+// strings => A string is piece of text. it can be a word , a sentence or even empty 
+// anything made of character.
+
+// let greeting = "Hello, World";
+// let name = 'Ali';
+// let empty = "";
+// let sentence = `My name is ${name}`
+// console.log(sentence);
+
+// constructor string  
+// let str = new String("Sam");
+// console.log(str);
+
+// string indexing 
+// let str = "samr";
+// console.log(str[2]);
+
+// let str = "sam";
+// for(let ch of str){
+//     console.log(ch);
+// }
+
+// let str = "Hello World";
+// let str2 = "";
+// for (let ch of str){
+//     // console.log(ch);
+//     str2 = str2 + ch + " "
+// }
+// console.log(str2);
+
+// properties => ye humain info deta he koi kam nhii karta kuch bta rha hota.
+// method => kuch kam kar deta he.
+
+// STRING METHODS 
+// let str = "Hello";
+// let str2 = "World";
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.trim());
+// console.log(str.concat(" ",str2, " ", "ali"));
+// console.log(str.includes("H"));
+// console.log(str.indexOf("r"));
+// console.log(str.charAt(5));
+// console.log(str.replace("H", "Y"));
+// console.log(str.replaceAll("e", "l"));
+
+// let userName = "@sam";
+// console.log(userName.slice(1));
+// console.log(userName.slice(1, 2));
+
+let str = "ali ahmad sam";
+console.log(str.split());
+console.log(str.split(""));
+console.log(str.split(" "));
+
+
+
+
+
