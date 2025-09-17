10c33660b4837cad938a362946163143a04d1b7f
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-17 15:42:28 +0500

    practice questions

---
+++ 05-functions/challenges.js
@@ -0,0 +1,20 @@
+// q1. 
+// function capitalizeFirstLetter(str){
+//     console.log(str.charAt().toUpperCase() + str.slice(1));
+// }
+// capitalizeFirstLetter("ali")
+
+// // q2. 
+// function showAlert(){
+//     alert("Please Login")
+// }
+// setTimeout(showAlert, 5000)
+
+
+// q3. 
+// let priceAfterDiscount = (price, discount)=>{
+//     let discountedPrice = price - price * (discount/100);
+//     return discountedPrice;
+// }
+// let ans = priceAfterDiscount(100,20);
+// console.log(ans);
