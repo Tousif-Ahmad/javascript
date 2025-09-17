c1015d67e5ac8a6d78142e0411c0f9abdf7418f7
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-17 15:21:47 +0500

    functions

---
+++ 05-functions/functions.js
@@ -0,0 +1,43 @@
+// function => it is a block of code designed to perform a particular task. it only runs when invoked / called.
+// function myFunc(n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum = sum + i;
+//   }
+//   console.log("sum", sum);
+// }
+// myFunc(5);
+// myFunc(10);
+// myFunc(3);
+
+
+// TYPES OF FUNCTIONS 
+// 1. function declaration / normal function 
+// function sayHi(){
+//     console.log("Hello");
+// }
+// sayHi();
+
+// 2. function expression 
+// let sayHi = function(){
+//     console.log("Hello");
+// }
+// sayHi();
+
+// 3. fat arrow function 
+// let sayHi = ()=>{
+//     console.log("hello");
+// }
+// sayHi();
+
+// 4. anonymous function 
+// setTimeout(function(){
+//     console.log("hello");
+// }, 1000)
+
+// 5. IIFE (immediately invoked function);
+// (function(){
+//     console.log("Heyyyy");
+// })()
+
+
