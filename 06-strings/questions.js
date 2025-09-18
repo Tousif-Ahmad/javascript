9e0e466e9c42766a4cf8a2144bce3b5fc806b6f9
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-18 15:54:07 +0500

    string questions

---
+++ 06-strings/questions.js
@@ -0,0 +1,23 @@
+// q1
+// function randomUserName(fullName){
+//     return "@" + fullName + "_" + fullName.length;
+// }
+// console.log(randomUserName("sam"));
+
+// q2. 
+// let str = prompt("Enter any string ");
+// let char = prompt("Enter any character ");
+// let count = 0;
+
+// for (let val of str){
+//     if (val.toLowerCase() === char.toLowerCase()){
+//         count++;
+//     }
+
+// }
+// console.log(count);
+
+
+// q3. 
+// let str = "Hello my name is sam";
+// console.log(str.split(" ").length);
