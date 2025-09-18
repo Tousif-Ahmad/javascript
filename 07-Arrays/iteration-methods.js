1e1292e58217c0c25227f1e011160fa741fa0315
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-18 22:13:15 +0500

    array iteration methods

---
+++ 07-Arrays/iteration-methods.js
@@ -0,0 +1,31 @@
+// MAP => Don't change original array 
+// let arr = [2,3,4,5,6];
+// let newArr = arr.map((elem, ind, ar)=>{
+//     // console.log(elem, ind, ar);
+//     // console.log(elem * 3);
+//     return elem * 2;
+// })
+// console.log(newArr);
+
+
+// FOREACH => changes the original array.
+// let arr = [2,3,4,5,6];
+// let newArr = arr.forEach((elem)=>{
+//     console.log(elem * 3);
+// })
+// console.log(newArr);
+// console.log(arr);
+
+// FILTER 
+// let arr = [2,3,4,5,6];
+// let newArr = arr.filter((elem)=>{
+//     return elem % 2 === 0;
+// })
+// console.log(newArr);
+
+// REDUCE 
+// let arr = [2,3,4,5,6];
+// let sum = arr.reduce((prevVal, currVal)=>{
+//     return prevVal + currVal;
+// }, 0)
+// console.log(sum);
