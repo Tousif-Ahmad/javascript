20e1bdf1874dfcd4fa78bdfb2ff005d92bf1a9ce
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-26 22:32:48 +0500

    Date Object

---
+++ 15-Date-Object/date.js
@@ -0,0 +1,35 @@
+// DATE OBJECT 
+// let now = new Date();  /* current date */
+// console.log(now);
+
+// let date = new Date("2025-09-26T08:34:00Z");  /* ISO STANDARD RECOMMENDED APPROACH */
+// console.log(date);
+
+// let currentTime = Date.now();
+// let tenMinutes = 1000 * 60 * 10;
+// let updatedTime = currentTime + tenMinutes;
+// // console.log(updatedTime);
+// // let date = new Date(updatedTime);
+// // console.log(date.toLocaleString());
+
+// let twoHours = 1000 * 60 * 60 * 2;
+// let newUpdatedTime = twoHours + updatedTime;
+// let date = new Date(newUpdatedTime);
+// console.log(date.toLocaleString());
+
+
+// CALCULATE DIFFERENCES BETWEEN DATES 
+// let date1 = new Date("2025-02-01T00:00:00");
+// let date2 = new Date("2023-02-01T00:00:00");
+// console.log(date1);
+// console.log(date2);
+
+// let diffInMilliSeconds = date1.getTime() - date2.getTime();
+// // diffInMilliSeconds / 1000 / 60 / 60;
+// console.log(diffInMilliSeconds / 1000);
+// console.log(diffInMilliSeconds / 1000 / 60);
+// console.log(diffInMilliSeconds / 1000 / 60 / 60);
+// console.log(diffInMilliSeconds / 1000 / 60 / 60 / 24);
+// console.log(diffInMilliSeconds / 1000 / 60 / 60 / 24 / 365);
+// console.log(diffInMilliSeconds);
+
