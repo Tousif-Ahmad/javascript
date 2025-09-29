01c7eee56ae4837dfa1e177f53397ba7d6c747aa
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-29 16:00:56 +0500

    programming paradigm

---
+++ 17-programming-paradigm/main.js
@@ -0,0 +1,88 @@
+// programming paradigm => tariqa he programming krne ka.
+// let arr = [1,2,3,45];
+// for (let i = 0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// arr.map((elem)=>{
+//     console.log(elem);
+// })
+// is me ye pta nhii ke internally kese kam krta he .. humain itna pta he ke kam krta he.
+
+// PROCEDURAL - procedure / functions
+
+// function consoleEnd(){
+//     console.log('Function is ended!!');
+// }
+// function checkNumber(num){
+//     if (num === 0) checkZero();
+//     else if (num > 0) positive();
+//     else{
+//         negative()
+//     }
+//     consoleEnd();
+// }
+
+// function positive(){
+//     console.log("Number is positive");
+// }
+
+// function checkZero(){
+//     console.log("Number is zero");
+// }
+// function negative(){
+//     console.log("Number is negative");
+// }
+
+// checkNumber(0)
+
+
+// STRUCTURED => functions /sequence  if else / loops 
+// function checkNumber(num){
+//     if (num === 0){
+//         console.log("Number is zero");
+//     }
+//     else if (num > 0){
+//         console.log("Number is positive");
+//     }
+//     else{
+//         console.log("Number is negative");
+//     }
+
+//     console.log("Program is ended!!");
+// }
+// checkNumber(2);
+
+
+// functional programming .........................
+// let arr = [3,4,5,6];
+// function double(){
+//     let newArr = arr.map((elem)=>{
+//         return elem * 2;
+//     })
+//     return newArr;
+// }
+// let result = double();
+// console.log(result);
+
+// OBJECT ORIENTED PROGRAMMING .......................
+// class Car{
+//     constructor(name){
+//         this.name = name;
+//     }
+//     start(){
+//         console.log("Starting the car....");
+//     }
+//     stop(){
+//         console.log("Stopping the car...");
+//     }
+
+// }
+// let Audi = new Car("Audi");
+// // console.log(Audi);
+// Audi.start();
+// Audi.stop();
+
+
+
+
