6b62d14bf8a9c1bb0b689ee640a3b2ca0e3cdd53
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-10-08 22:33:51 +0500

    async

---
+++ async-programming/sync-async/async.js
@@ -0,0 +1,23 @@
+// function getData(data){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             resolve({
+//                 name : "sam",
+//                 data : data,
+//             })
+//         }, 2000);
+//     })
+// }
+// async function fetchData(){
+//     let data1 = await getData(1);
+//     console.log(data1);
+//     let data2 = await getData(2);
+//     console.log(data2);
+//     let data3 = await getData(3);
+//     console.log(data3);
+//     let data4 = await getData(4);
+//     console.log(data4);
+// }
+// fetchData()
+
+
