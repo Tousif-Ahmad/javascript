2769119b746a69777acc6772ee830190a0fc7871
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-10-08 15:40:03 +0500

    sync and async

---
+++ async-programming/sync-async/script.js
@@ -0,0 +1,24 @@
+// async function fetchQuote() {
+//     let response = await fetch("https://zenquotes.io/api/random");
+//     let data = await response.json();
+//     console.log(data);
+
+// }
+//  fetchQuote();
+
+// console.log("task 1");
+// setTimeout(() => {
+//     console.log("Task 2");
+// }, 2000);
+// console.log("task 3");
+
+console.log("1");
+setTimeout(() => {
+  console.log("2");
+}, 5000);
+console.log("3");
+console.log("4");
+setTimeout(() => {
+  console.log("5");
+}, 3000);
+console.log("6");
